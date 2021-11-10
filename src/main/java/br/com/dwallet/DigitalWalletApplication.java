e19dcf0c7f2620d0fc52ae0d4f897e6f87ce7fae
Mateus Felipe Teixeira <mateusteixeira_@hotmail.com>
2021-11-09 22:43:19 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/dwallet/DigitalWalletApplication.java
@@ -0,0 +1,13 @@
+package br.com.dwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dwallet/DigitalWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
